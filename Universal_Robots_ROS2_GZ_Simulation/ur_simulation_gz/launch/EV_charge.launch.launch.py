c33bcb5ac47b579372828c493988f65a9bb7a6e9
ILIAS STATHAKOS <56887619+ilias-stath@users.noreply.github.com>
2025-10-14 20:21:15 +0300

    Update and rename ur_sim_application.launch.py to EV_charge.launch.launch.py

---
+++ Universal_Robots_ROS2_GZ_Simulation/ur_simulation_gz/launch/EV_charge.launch.launch.py
@@ -0,0 +1,44 @@
+import launch
+import os
+from launch import LaunchDescription
+from launch.actions import TimerAction
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # 1. move_moveit node
+    move_moveit_node = Node(
+        package='ur_simulation_gz',
+        executable='move_moveit',
+        name='move_moveit',
+        output='screen',
+        parameters=[{"use_sim_time": True}],
+    )
+
+    # 2. yolo_model node
+    yolo_node_node = Node(
+        package='yolo_model',
+        executable='yolo_node',
+        name='yolo_node',
+        output='screen'
+    )
+
+    # 3. main_logic node
+    main_logic_node = Node(
+        package='ur_simulation_gz',
+        executable='main_logic',
+        name='main_logic',
+        output='screen'
+    )
+
+    # Launch order with delays
+    # delayed_yolo_node = TimerAction(period=10.0, actions=[yolo_node_node])
+    delayed_move_moveit_node = TimerAction(period=10.0, actions=[move_moveit_node])
+    delayed_main_logic = TimerAction(period=20.0, actions=[main_logic_node])  # 5s after yolo
+
+    return LaunchDescription([
+        yolo_node_node,
+        delayed_move_moveit_node,
+        delayed_main_logic
+    ])
